6f623a51f7c315e6e7b93c2866e82ad43bd88cd1
PriscilaNetto <priscila.nnetto@hotmail>
2021-12-15 20:08:38 -0300

    Add project files.

---
+++ aula38_exer_sem_orient_obj/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace aula38_exer_sem_orient_obj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y; //variaveis do tipo Triangulo (classe)
+
+            x = new Triangulo();
+            y = new Triangulo(); //instanciação da variavel 
+            
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // dessa forma cada atributo será alocado na memória
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //vai receber o resultado de acordo com a chamada do método dentro do obj X.
+            double areaX = x.CalculateTrianguleArea(); 
+
+            double areaY = y.CalculateTrianguleArea();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior área é X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área é Y");
+            }
+
+        }
+    }
+}
---
+++ aula38_exer_sem_orient_obj/Triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aula38_exer_sem_orient_obj
+{
+    class Triangulo
+    {
+        //dentro da classe existe os membros que podem ser atributos e métodos como a seguir:
+
+        //são atributos da classe
+        public double A; //o public pode ser usado em outros arquivos - encapsulamento;
+        public double B; 
+        public double C;
+
+        // o método criado precisa ficar dentro das chaves da classe triangulo
+        //o double é o tipo de dado que retorna, para não retornar nada é colocado 'void'
+        //não foi usado nenhum paramentro pq todos os dados usados já existem e pertencem à classe Triangulo.
+        public double CalculateTrianguleArea()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+        }
+
+
+    }
+}
